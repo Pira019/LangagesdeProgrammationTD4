a681706c572c99902b8827d61b7f1ca94cd23e4f
pires lufungula <pires00243@gmail.com>
2024-02-28 01:39:29 -0500

    sol ok

--- main.py
+++ main.py
@@ -4,29 +4,7 @@ from typing import TypeVar, Callable
 
 T = TypeVar('T', int, float)
 
-def somme_jusqua(n:int)->int:
-  somme = 0
-  k = 0
-  while k <= n:
-    somme = somme + k
-    k = k + 1
-  return somme
-
-def somme_carres(n:int)->int:
-  somme = 0
-  k = 0
-  while k <= n:
-    somme = somme + k*k
-    k = k + 1
-  return somme
-
-def somme_pi(n:int)->float:
-  somme = 0
-  k = 0
-  while k <= n:
-    somme = somme + (-1)**k * 4 / (2*k +1)
-    k = k + 1
-  return somme
+ 
 
 def somme_parametree(n:int, f:Callable[[int],T])->T:
   somme:T = 0
@@ -40,6 +18,6 @@ somme_jusqua_2:Callable[[int],int] = lambda n : somme_parametree(n, lambda x:x)
 somme_carres_2:Callable[[int],int] = lambda n : somme_parametree(n, lambda x:x*x)
 somme_pi_2:Callable[[int],float] = lambda n : somme_parametree(n, lambda x: (-1)**x * 4/ (2*x +1))
 
-print("somme_jusqua(1000)=", somme_parametree(1000, somme_jusqua_2), somme_jusqua_2(1000))
-print("somme_carres(1000)=", somme_carres(2), somme_parametree(1000, somme_carres_2), somme_carres_2(2))
-print("somme_pi(1000)=", somme_pi(1000), somme_parametree(1000,somme_pi_2), somme_pi_2(1000))
+print("somme_jusqua(1000)=", somme_jusqua_2(1000))
+print("somme_carres(1000)=", somme_carres_2(100))
+print("somme_pi(1000)=", somme_pi_2(1000))
